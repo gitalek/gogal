3a01bf998e6c1b0562b7afc82dba610bfa8a7244
Alexander <shalek.edu@mail.ru>
2021-03-06 20:48:38 +1000

    logic:controller(users): stub Create action

--- controllers/users.go
+++ controllers/users.go
@@ -17,8 +17,14 @@ func NewUsers() (*Users, error) {
 	return &Users{NewView: v}, nil
 }
 
+// New processes the GET /new route
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// Create processes the POST /signup route
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is a temporary response."))
 }
