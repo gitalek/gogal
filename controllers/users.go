6ac8438bfb586898562b0610f638a5ba7015be7e
Alexander <shalek.edu@mail.ru>
2021-03-06 18:37:07 +1000

    logic:controller(Users): define constructor func and New method

--- controllers/users.go
+++ controllers/users.go
@@ -1,3 +1,24 @@
 package controllers
 
-type Users struct {}
+import (
+	"github.com/gitalek/gogal/views"
+	"net/http"
+)
+
+type Users struct {
+	NewView *views.View
+}
+
+func NewUsers() (*Users, error) {
+	v, err := views.NewView("bootstrap", "views/users/new.gohtml")
+	if err != nil {
+		return nil, err
+	}
+	return &Users{NewView: v}, nil
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
