45e06fbd185a362cf82e921da3624bd687b1e972
Alexander <shalek.edu@mail.ru>
2021-03-08 19:28:28 +1000

    models(user): implement ByEmail method

--- models/users.go
+++ models/users.go
@@ -58,6 +58,16 @@ func (us *UserService) ByID(id uint) (*User, error) {
 	return &user, nil
 }
 
+func (us *UserService) ByEmail(email string) (*User, error) {
+	var user User
+	db := us.db.Where("email = ?", email)
+	err := first(db, &user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
 func first(db *gorm.DB, dst interface{}) error {
 	err := db.First(dst).Error
 	if err == gorm.ErrRecordNotFound {
