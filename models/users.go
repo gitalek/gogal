8f62c420a1beb44354e50d0a2ecf5ae3858d565d
Alexander <shalek.edu@mail.ru>
2021-03-08 19:20:52 +1000

    models(user): add filrst helper and refactor ByID method

--- models/users.go
+++ models/users.go
@@ -50,13 +50,18 @@ func (us *UserService) Create(user *User) error {
 // probably result in a 500 error.
 func (us *UserService) ByID(id uint) (*User, error) {
 	var user User
-	err := us.db.Where("id = ?", id).First(&user).Error
-	switch err {
-	case nil:
-		return &user, nil
-	case gorm.ErrRecordNotFound:
-		return nil, ErrNotFound
-	default:
+	db := us.db.Where("id = ?", id)
+	err := first(db, &user)
+	if err != nil {
 		return nil, err
 	}
+	return &user, nil
+}
+
+func first(db *gorm.DB, dst interface{}) error {
+	err := db.First(dst).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrNotFound
+	}
+	return err
 }
