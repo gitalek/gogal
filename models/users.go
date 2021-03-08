380111b55a7f64f674928cbee3982302c42042f3
Alexander <shalek.edu@mail.ru>
2021-03-08 19:01:54 +1000

    models: define UserService as implementation of User model interface

--- models/users.go
+++ models/users.go
@@ -1,9 +1,58 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	// ErrNotFound is returned when a resource cannot be found in the database
+	ErrNotFound = errors.New("models: resource not found")
+)
 
 type User struct {
 	gorm.Model
 	Name string
 	Email string `gorm:"not null;unique_index"`
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(connStr string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{db: db}, nil
+}
+
+// Close method closes the UserService database connection.
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+// DestructiveReset method drops the user table and rebuilds it.
+// Used in development env.
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
+}
+
+// As a general rule, any error but ErrNotFound should
+// probably result in a 500 error.
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
 }
