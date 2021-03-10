eb9eee2437e43e40265317db44968c7aeecbc35d
Alexander <shalek.edu@mail.ru>
2021-03-10 17:00:42 +1000

    define UserDB interface to interact with the users database

--- models/users.go
+++ models/users.go
@@ -23,6 +23,23 @@ var (
 	hmacSecretKey = "secret-hmac-key"
 )
 
+// UserDB is used to interact with the users database.
+type UserDB interface {
+	// Query for single users.
+	ByID(id uint) (*User, error)
+	ByEmail(email string) (*User, error)
+	ByRemember(token string) (*User, error)
+	// Alter users.
+	Create(user *User) error
+	Update(user *User) error
+	Delete(id uint) error
+	// Close is used to close a DB connection.
+	Close() error
+	// Migration helpers
+	AutoMigrate() error
+	DestructiveReset() error
+}
+
 type User struct {
 	gorm.Model
 	Name         string
