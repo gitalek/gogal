d5bff3e35292cf1a0a08956732abc9584e70f69d
Alexander <shalek.edu@mail.ru>
2021-03-11 22:15:07 +1000

    validation(galleries): define validation fn type and set validations helper

---
+++ models/gallery_validator.go
@@ -0,0 +1,12 @@
+package models
+
+type galleryValFn func(*Gallery) error
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
+}
