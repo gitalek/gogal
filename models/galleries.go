0b8f5ee8cf9753345f3cc43a033d914ac7de1fe7
Alexander <shalek.edu@mail.ru>
2021-03-11 20:40:11 +1000

    models: define GalleryServire and GalleryDB interfaces; implement with galleryGorm type

--- models/galleries.go
+++ models/galleries.go
@@ -7,3 +7,20 @@ type Gallery struct {
 	UserID uint   `gorm:"not null;index"`
 	Title  string `gorm:no null`
 }
+
+type GalleryService interface {
+	GalleryDB
+}
+
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
+}
+
+func (gg *galleryGorm) Create(gallery *Gallery) error {
+	//todo: to implement
+	return nil
+}
