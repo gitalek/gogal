ea3d114b2f81406c723283a4a1fe4b02f9ae630e
Alexander <shalek.edu@mail.ru>
2021-03-13 17:23:12 +1000

    models(gallery): update GalleryDB interface with ByID method and implement this one

--- models/galleries.go
+++ models/galleries.go
@@ -14,6 +14,7 @@ type GalleryService interface {
 
 type GalleryDB interface {
 	Create(gallery *Gallery) error
+	ByID(id uint) (*Gallery, error)
 }
 
 type galleryService struct {
@@ -42,4 +43,14 @@ var _ GalleryDB = &galleryGorm{}
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+func (gg *galleryGorm) ByID(id uint) (*Gallery, error) {
+	var gallery Gallery
+	db := gg.db.Where("id = ?", id)
+	err := first(db, &gallery)
+	if err != nil {
+		return nil, err
+	}
+	return &gallery, nil
 }
