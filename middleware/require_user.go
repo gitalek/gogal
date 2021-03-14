5beb095db970f20d4aa1aeccfd009e4121de8f84
Alexander <shalek.edu@mail.ru>
2021-03-14 17:32:54 +1000

    mw: extract two middlewares from requireUser: one for user context binding and one for rediricting unsigned users

--- middleware/require_user.go
+++ middleware/require_user.go
@@ -6,29 +6,46 @@ import (
 	"net/http"
 )
 
-type RequireUser struct {
+type RequireUser struct {}
+
+func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+    	user := context.User(r.Context())
+    	if user == nil {
+    		http.Redirect(w, r, "/login", http.StatusFound)
+    		return
+		}
+        next(w, r)
+    }
+}
+
+func (mw *RequireUser) Apply(next http.Handler) http.HandlerFunc {
+	return mw.ApplyFn(next.ServeHTTP)
+}
+
+type User struct {
 	models.UserService
 }
 
-func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
- 		cookie, err := r.Cookie("remember_token")
+func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("remember_token")
 		if err != nil {
-			http.Redirect(w, r, "/login", http.StatusFound)
+			next(w, r)
 			return
 		}
-        user, err := mw.UserService.ByRemember(cookie.Value)
+		user, err := mw.UserService.ByRemember(cookie.Value)
 		if err != nil {
-			http.Redirect(w, r, "/login", http.StatusFound)
+			next(w, r)
 			return
 		}
 		ctx := r.Context()
 		ctx = context.WithUser(ctx, user)
 		r = r.WithContext(ctx)
-        next(w, r)
-    })
+		next(w, r)
+	}
 }
 
-func (mw *RequireUser) Apply(next http.Handler) http.HandlerFunc {
+func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 	return mw.ApplyFn(next.ServeHTTP)
 }
