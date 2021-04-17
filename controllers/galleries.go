510ffcfdf0b5a9cf93b23b73ba3ef15b6fe47049
Alexander <shalek.edu@mail.ru>
2021-04-17 19:59:22 +1000

    controllers(galleries): define the ImageUpload method

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,18 +1,23 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gitalek/gogal/context"
 	"github.com/gitalek/gogal/models"
 	"github.com/gitalek/gogal/views"
 	"github.com/gorilla/mux"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 )
 
 const (
-	IndexGalleries = "index_galleries"
-	ShowGallery    = "show_gallery"
-	EditGallery    = "edit_gallery"
+	IndexGalleries  = "index_galleries"
+	ShowGallery     = "show_gallery"
+	EditGallery     = "edit_gallery"
+	maxMultiPartMem = 1 << 20 // 1 megabyte
 )
 
 type Galleries struct {
@@ -194,4 +199,72 @@ func (g *Galleries) Index(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
 	vd.Yield = galleries
 	g.IndexView.Render(w, r, vd)
+}
+
+// POST /galleries/:id/images
+func (g *Galleries) ImageUpload(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+	// TODO: Finish implementing this
+	var vd views.Data
+	vd.Yield = gallery
+	err = r.ParseMultipartForm(maxMultiPartMem)
+	if err != nil {
+		// If we can't parse the form just render an error alert on the
+		// edit gallery page
+		vd.SetAlert(err)
+		g.EditView.Render(w, r, vd)
+		return
+	}
+
+	galleryPath := filepath.Join("images", "galleries", fmt.Sprintf("%v", gallery.ID))
+	err = os.MkdirAll(galleryPath, 0755)
+	if err != nil {
+		vd.SetAlert(err)
+		g.EditView.Render(w, r, vd)
+		return
+	}
+
+	// Iterate over uploaded files
+	files := r.MultipartForm.File["images"]
+	for _, f := range files {
+		// Open the uploaded file
+		file, err := f.Open()
+		if err != nil {
+			vd.SetAlert(err)
+			g.EditView.Render(w, r, vd)
+			return
+		}
+		defer file.Close()
+
+		// Create a destination file
+		dst, err := os.Create(filepath.Join(galleryPath, f.Filename))
+		if err != nil {
+			vd.SetAlert(err)
+			g.EditView.Render(w, r, vd)
+			return
+		}
+		defer dst.Close()
+
+		// Copy uploaded file data to the destination file
+		_, err = io.Copy(dst, file)
+		if err != nil {
+			vd.SetAlert(err)
+			g.EditView.Render(w, r, vd)
+			return
+		}
+	}
+
+	vd.Alert = &views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Images successfully uploaded!",
+	}
+	g.EditView.Render(w, r, vd)
 }
