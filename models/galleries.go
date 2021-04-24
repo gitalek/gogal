4dc2d5566fe6dcde279ff23ec87c8cb8441b08c2
Alexander <shalek.edu@mail.ru>
2021-04-24 19:26:46 +1000

    models(galleries): define ImagesSplitN method on Gallery raw data type

--- models/galleries.go
+++ models/galleries.go
@@ -9,6 +9,23 @@ type Gallery struct {
 	Images []string `gorm:"-"`
 }
 
+func (g *Gallery) ImagesSplitN(n int) [][]string {
+	// Create out 2D slice.
+	ret := make([][]string, n)
+	// Create the inner slices - we need N of them, and we will
+	// start them with a size of 0.
+	for i := 0; i < n; i++ {
+		ret[i] = make([]string, 0)
+	}
+	// Iterate over our images, using the index % n to determine
+	// which of the slices in ret to add the image to.
+	for i, img := range g.Images {
+		bucket := i % n
+		ret[bucket] = append(ret[bucket], img)
+	}
+	return ret
+}
+
 type GalleryService interface {
 	GalleryDB
 }
