b5a4dd9d44620544ca463b2d77728aee20c9ace4
Alexander <shalek.edu@mail.ru>
2021-04-27 04:39:07 +1000

    config: define PostgresConfig type

---
+++ config.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type PostgresConfig struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+}
+
+func (c PostgresConfig) Dialect() string {
+	return "postgres"
+}
+
+func (c PostgresConfig) ConnectionInfo() string {
+	// We are going to provide two potential connection info strings
+	// based on whether a password is present.
+	if c.Password == "" {
+		return fmt.Sprintf(
+			"host=%s port=%d user=%s dbname=%s sslmode=dilable",
+			c.Host, c.Port, c.User, c.Name,
+		)
+	}
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=dilable",
+		c.Host, c.Port, c.User, c.Password, c.Name,
+	)
+}
