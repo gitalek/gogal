43a56a723f20d24306e18565bd673f9bbf53c6ba
Alexander <shalek.edu@mail.ru>
2021-04-29 01:46:03 +1000

    populate Services type with functional options

--- models/services.go
+++ models/services.go
@@ -9,19 +9,49 @@ type Services struct {
 	db      *gorm.DB
 }
 
-func NewServices(dialect, connStr string) (*Services, error) {
-	db, err := gorm.Open(dialect, connStr)
-	if err != nil {
-		return nil, err
+type ServicesConfig func(*Services) error
+
+func NewServices(cfgs ...ServicesConfig) (*Services, error) {
+	var s Services
+	for _, cfg := range cfgs {
+		if err := cfg(&s); err != nil {
+			return nil, err
+		}
 	}
-	db.LogMode(true)
-	return &Services{
-		User:    NewUserService(db),
-		Gallery: NewGalleryService(db),
-		Image:   NewImageService(),
-		db:      db,
-	}, nil
+	return &s, nil
 }
+
+func WithGorm(dialect, connectionInfo string) ServicesConfig {
+	return func(s *Services) error {
+		db, err := gorm.Open(dialect, connectionInfo)
+		if err != nil {
+			return err
+		}
+		s.db = db
+		return nil
+	}
+}
+
+func WithLogMode(mode bool) ServicesConfig {
+	return func(s *Services) error {
+		s.db.LogMode(mode)
+		return nil
+	}
+}
+
+//func NewServices(dialect, connStr string) (*Services, error) {
+//	db, err := gorm.Open(dialect, connStr)
+//	if err != nil {
+//		return nil, err
+//	}
+//	db.LogMode(true)
+//	return &Services{
+//		User:    NewUserService(db),
+//		Gallery: NewGalleryService(db),
+//		Image:   NewImageService(),
+//		db:      db,
+//	}, nil
+//}
 
 // Close method closes the database connection.
 func (s *Services) Close() error {
